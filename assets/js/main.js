2e5bb8e414d7231411844bc2851e24e79371345b
Danilo Bilanoski <dubssta@gmail.com>
2019-10-15 21:50:50 +0200

    Repurposed whole site to small clothing web store to test out Jekyll platform better.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,11 @@
-console.log("Loaded");
+
+const buyButton = document.querySelector(".buy-button") || null;
+if(buyButton != null) {
+  buyButton.addEventListener("click", function() {
+    const productTitle = buyButton.parentElement.childNodes[1].innerHTML;
+    const grammar = productTitle.includes("shoes") || productTitle.includes("boots") ? "are" : "is";
+  
+  alert(`${productTitle} ${grammar} added to fake cart.`)
+})
+}
+
